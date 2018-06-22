ca230fe95d48453f0cbae392be168c60c64a0cfd
Nicole Rauch <info@nicole-rauch.de>
2018-06-22 15:45:32 +0200

    Adding TypeScript types.

---
+++ index.d.ts
@@ -0,0 +1,9 @@
+
+
+export function extract(input : string) : string | string[];
+
+export function parse(input : string, options : object) : object;
+
+export function stringify(obj : object, options : object) : string;
+
+export function parseUrl(input : string, options : object) : object;
