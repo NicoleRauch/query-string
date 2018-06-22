525434f2ebc0d77ea165b5e37f160211f008fe90
Nicole Rauch <info@nicole-rauch.de>
2018-06-22 16:07:11 +0200

    All of the options params are optional.

--- index.d.ts
+++ index.d.ts
@@ -2,8 +2,8 @@
 
 export function extract(input : string) : string | string[];
 
-export function parse(input : string, options : object) : object;
+export function parse(input : string, options? : object) : object;
 
-export function stringify(obj : object, options : object) : string;
+export function stringify(obj : object, options? : object) : string;
 
 export function parseUrl(input : string, options? : object) : object;
